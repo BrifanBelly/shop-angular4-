58bf5a529d829d791aea43958d7417cb9ee52d9f
BrifanBelly <brifankle@gmail.com>
2018-01-24 14:44:13 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   config: {
-    apiKey: 'AIzaSyB1_VBNWSi7-2bKFv4EpGx6LfqhniKKhuY',
-    authDomain: 'oshop-2d115.firebaseapp.com',
-    databaseURL: 'https://oshop-2d115.firebaseio.com',
-    projectId: 'oshop-2d115',
-    storageBucket: 'oshop-2d115.appspot.com',
-    messagingSenderId: '433959952843'
+// firebase configuration
   }
 };
